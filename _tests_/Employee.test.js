7d307a1b822cb1169fb44fefd4ed22b05c6d9be5
jack-ie <jackie919@outlook.com>
2022-01-31 14:11:53 -0600

    added engineer test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,15 +2,18 @@
 const Employee = require('../lib/Employee');
 
 // Creates employee object 
-test('Employee Object', ()=>{
+test('Employee Object', () => {
     const employee = new Employee('Jackeline', '90', 'jackietest@testmail.com');
-    it('getName = name', ()=>{
+    // gets employee name
+    it('getName = name', () => {
         expect(employee.getName()).toEqual('Jackeline');
     })
-    it("getID = id", () => {
+    // gets employee id
+    it('getID = id', () => {
         expect(employee.getId()).toEqual('90');
-      })
-      it("getEmail = email", () => {
+    })
+    // gets employee email
+    it('getEmail = email', () => {
         expect(employee.getEmail()).toEqual('jackietest@testmail.com');
-      })
+    })
 });
---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,21 @@
+const Engineer = require('../lib/Engineer');
+
+describe('engineer object', () => {
+    const engineer = new Engineer('Jackeline', '90', 'jackietest@testmail.com', 'mygithub');
+    // gets engineer name
+    it('getName = name', () => {
+        expect(engineer.getName()).toEqual('Jackeline');
+    })
+    // gets engineer id
+    it('getID = id', () => {
+        expect(engineer.getId()).toEqual('90');
+    })
+    // gets engineer email
+    it('getEmail = email', () => {
+        expect(engineer.getEmail()).toEqual('jackietest@testmail.com');
+    })
+    // gets engineer github username
+    it('getGithub = github', () => {
+        expect(engineer.getGithub()).toEqual('mygithub');
+    })
+});
