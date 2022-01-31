e2aa95cb5708df16fa3174819e3dd9609d14a32c
jack-ie <jackie919@outlook.com>
2022-01-31 15:04:19 -0600

    added function to write to html

--- index.js
+++ index.js
@@ -101,3 +101,17 @@ const internQuestions = [
 ];
 
 // function runs questions
+
+// function to generate HTML page file using file system 
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+            // when the profile has been created 
+        } else {
+            console.log('Your team profile has been created. Please see index.html')
+        }
+    })
+};
